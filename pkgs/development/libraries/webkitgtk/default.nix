855549a1969b6ccc214a5258c835cdb6a6d64948
Bobby Rong <rjl931189261@126.com>
2023-09-30 00:14:34 +0800

    webkitgtk: 2.42.0 → 2.42.1
    https://github.com/WebKit/WebKit/compare/webkitgtk-2.42.0...webkitgtk-2.42.1
    https://webkitgtk.org/security/WSA-2023-0009.html

--- pkgs/development/libraries/webkitgtk/default.nix
+++ pkgs/development/libraries/webkitgtk/default.nix
@@ -70,7 +70,7 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "webkitgtk";
-  version = "2.42.0";
+  version = "2.42.1";
   name = "${finalAttrs.pname}-${finalAttrs.version}+abi=${if lib.versionAtLeast gtk3.version "4.0" then "6.0" else "4.${if lib.versions.major libsoup.version == "2" then "0" else "1"}"}";
 
   outputs = [ "out" "dev" "devdoc" ];
@@ -81,7 +81,7 @@ stdenv.mkDerivation (finalAttrs: {
 
   src = fetchurl {
     url = "https://webkitgtk.org/releases/webkitgtk-${finalAttrs.version}.tar.xz";
-    hash = "sha256-go+Vk1hh+uWD+48q5Yz2TGPBeK4rfC1vcwcIE61k7Rs=";
+    hash = "sha256-b0H6yZidPuUcCMSN4dQ5ze3ey8dX40thgJh9mbFtJJk=";
   };
 
   patches = lib.optionals stdenv.isLinux [
