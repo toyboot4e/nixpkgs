70db7411f4d125eef82c356c185ded9f068f7868
Bobby Rong <rjl931189261@126.com>
2023-10-02 20:44:21 +0800

    fixup! glib: 2.76.4 → 2.77.1
    This comment seems out-of-date to me, e.g. gtester-report is compatible
    with python3, e.g. we have a gen-visibility-macros.py which needs python3, etc

--- pkgs/development/libraries/glib/default.nix
+++ pkgs/development/libraries/glib/default.nix
@@ -24,17 +24,12 @@
 
 assert stdenv.isLinux -> util-linuxMinimal != null;
 
-# TODO:
-# * Make it build without python
-#     Problem: an example (test?) program needs it.
-#     Possible solution: disable compilation of this example somehow
-#     Reminder: add 'sed -e 's@python2\.[0-9]@python@' -i
-#       $out/bin/gtester-report' to postInstall if this is solved
 /*
+  * TODO:
   * Use --enable-installed-tests for GNOME-related packages,
       and use them as a separately installed tests run by Hydra
       (they should test an already installed package)
-      https://wiki.gnome.org/GnomeGoals/InstalledTests
+      https://wiki.gnome.org/Initiatives/GnomeGoals/InstalledTests
   * Support org.freedesktop.Application, including D-Bus activation from desktop files
 */
 let
