4d215c3c6c34606e9827a24dcf8970f5d1a97055
toyboot4e <toyboot4e@gmail.com>
2025-07-03 00:24:34 +0900

    python3Packages.pyobjc-framework-Cocoa: 11.0 -> 11.1

--- pkgs/development/python-modules/pyobjc-framework-Cocoa/default.nix
+++ pkgs/development/python-modules/pyobjc-framework-Cocoa/default.nix
@@ -5,18 +5,19 @@
   lib,
   pyobjc-core,
   setuptools,
+  xcbuild,
 }:
 
 buildPythonPackage rec {
   pname = "pyobjc-framework-Cocoa";
-  version = "11.0";
+  version = "11.1";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "ronaldoussoren";
     repo = "pyobjc";
     tag = "v${version}";
-    hash = "sha256-RhB0Ht6vyDxYwDGS+A9HZL9ySIjWlhdB4S+gHxvQQBg=";
+    hash = "sha256-2qPGJ/1hXf3k8AqVLr02fVIM9ziVG9NMrm3hN1de1Us=";
   };
 
   sourceRoot = "${src.name}/pyobjc-framework-Cocoa";
@@ -26,21 +27,26 @@ buildPythonPackage rec {
   buildInputs = [
     darwin.libffi
     darwin.DarwinTools
+    xcbuild
   ];
 
   nativeBuildInputs = [
     darwin.DarwinTools # sw_vers
+    xcbuild
   ];
+
+  dependencies = [ pyobjc-core ];
 
   # See https://github.com/ronaldoussoren/pyobjc/pull/641. Unfortunately, we
   # cannot just pull that diff with fetchpatch due to https://discourse.nixos.org/t/how-to-apply-patches-with-sourceroot/59727.
   postPatch = ''
     substituteInPlace pyobjc_setup.py \
+      --replace-fail "-productversion" "-productVersion" \
       --replace-fail "-buildversion" "-buildVersion" \
-      --replace-fail "-productversion" "-productVersion"
+      --replace-fail "/usr/bin/sw_vers" "${darwin.DarwinTools}/bin/sw_vers" \
+      --replace-fail '"sw_vers"' '"${darwin.DarwinTools}/bin/sw_vers"' \
+      --replace-fail "/usr/bin/xcrun" "${xcbuild.xcrun}/bin/xcrun"
   '';
-
-  dependencies = [ pyobjc-core ];
 
   env.NIX_CFLAGS_COMPILE = toString [
     "-I${darwin.libffi.dev}/include"
